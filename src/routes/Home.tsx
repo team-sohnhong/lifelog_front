07485bfb24f98dbcd6035f6d9fdfad2a37d09d3b
MJsystem <audwns232@naver.com>
2022-01-22 13:08:33 +0900

    Do CRA and adjust Ts

---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+import { AppBar } from "@mui/material";
+import exp from "constants";
+import Dashboard from "../components/Dashboard";
+
+function Home() {
+  return <AppBar />;
+}
+
+export default Home;
